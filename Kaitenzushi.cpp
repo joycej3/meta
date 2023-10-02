f1924d712e1f2a7ce0d4bc3aa28ab4343859b437
John Joyce <65554933+joycej3@users.noreply.github.com>
2023-10-02 01:21:13 +0100

    Update Kaitenzushi.cpp

--- Kaitenzushi.cpp
+++ Kaitenzushi.cpp
@@ -1,21 +1,20 @@
 
 int getMaximumEatenDishCount(int N, vector<int> D, int K) {
     unordered_set<int> index;
-    deque<int> stored;
+    queue<int> stored;
     int count = 0;
 
     for (int d : D) {
         if (index.find(d) == index.end()) {
             if (index.size() == K) {
                 int d_old = stored.front();
-                stored.pop_front();
+                stored.pop();
                 index.erase(d_old);
             }
             index.insert(d);
-            stored.push_back(d);
-            count++;a
+            stored.push(d);
+            count++;
         }
     }
-
     return count;
 }
